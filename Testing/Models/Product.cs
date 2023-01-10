293b4c9e68610bb26ededb755956f39a215c9bdc
macieowens <112588931+macieowens@users.noreply.github.com>
2023-01-10 11:39:11 -0600

    initial commit

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public double Price { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
